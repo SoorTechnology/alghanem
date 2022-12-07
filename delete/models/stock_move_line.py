0a460c3bec5386da5f0023b2e17a0c39aeee9429
CybroOdooDev <cybroodoodev@gmail.com>
2022-12-07 11:37:40 +0530

    [Removing product category]

--- delete/models/stock_move_line.py
+++ delete/models/stock_move_line.py
@@ -4,6 +4,6 @@ from odoo import fields, models
 class ProductCategory(models.Model):
     _inherit = 'product.template'
 
-    def delete(self):
+    def action(self):
         for rec in self:
             rec.categ_id = 102
